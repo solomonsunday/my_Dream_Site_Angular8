e75eeb0355dea4631e18de8690e893b61df26230
unknown <richard.okoye@netopng.com>
2020-01-15 01:56:48 +0100

    classWork

---
+++ server/generate.js
@@ -0,0 +1,37 @@
+var faker = require('faker');
+var database = {
+  products: []
+};
+
+for (var i = 1; i <= 300; i++) {
+  database.products.push({
+    id: i,
+    name: faker.commerce.productName(),
+    descriptioin: faker.lorem.sentences(),
+    price: faker.commerce.price(),
+    imageUrl: "https://source.unsplash.com/1600x900/?product",
+    quantity: faker.random.number()
+  });
+}
+
+console.log(JSON.stringify(database));
+
+
+
+// var cart = {
+//     cartitem: []
+// };
+
+// for(vari=1; i<cartitem; i++){
+//     cart.cartitem.push({
+
+//     })
+// }
+
+// var faker = {
+//     commerce : {
+//         productName : Nike,
+//         price: 900,
+
+//     }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
